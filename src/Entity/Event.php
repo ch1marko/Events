cd6091c78441251a9c15147a5243b01d13a46328
Marko <marko.dzomba.info@gmail.com>
2022-12-02 16:49:26 +0100

    initial, pics, style

---
+++ src/Entity/Event.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventRepository::class)]
+class Event
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $start_time = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $picture = null;
+
+    #[ORM\Column]
+    private ?int $capacity = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private ?int $phone_number = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $city = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $street_name = null;
+
+    #[ORM\Column]
+    private ?int $house_number = null;
+
+    #[ORM\Column]
+    private ?int $zip_code = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $event_url = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getStartTime(): ?\DateTimeInterface
+    {
+        return $this->start_time;
+    }
+
+    public function setStartTime(\DateTimeInterface $start_time): self
+    {
+        $this->start_time = $start_time;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    public function getCapacity(): ?int
+    {
+        return $this->capacity;
+    }
+
+    public function setCapacity(int $capacity): self
+    {
+        $this->capacity = $capacity;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?int
+    {
+        return $this->phone_number;
+    }
+
+    public function setPhoneNumber(int $phone_number): self
+    {
+        $this->phone_number = $phone_number;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getStreetName(): ?string
+    {
+        return $this->street_name;
+    }
+
+    public function setStreetName(string $street_name): self
+    {
+        $this->street_name = $street_name;
+
+        return $this;
+    }
+
+    public function getHouseNumber(): ?int
+    {
+        return $this->house_number;
+    }
+
+    public function setHouseNumber(int $house_number): self
+    {
+        $this->house_number = $house_number;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?int
+    {
+        return $this->zip_code;
+    }
+
+    public function setZipCode(int $zip_code): self
+    {
+        $this->zip_code = $zip_code;
+
+        return $this;
+    }
+
+    public function getEventUrl(): ?string
+    {
+        return $this->event_url;
+    }
+
+    public function setEventUrl(string $event_url): self
+    {
+        $this->event_url = $event_url;
+
+        return $this;
+    }
+}
---
+++ src/Form/EventType.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name',  TextType::class, ["attr"=>["placeholder" => "Please type", "class" => "form-control  w-75 mb-2" ]])
+            ->add('date', DateTimeType::class, ["attr" =>["class"=>"form-control  w-75 mb-2",'format' => 'yyyy-MM-dd']])
+            ->add('start_time', DateTimeType::class, ["attr" =>["class"=>"form-control  w-75 mb-2"]])
+            ->add('description', TextareaType::class,  ["attr"=>["placeholder" => "Please type a description...", "class" => "form-control  w-75 mb-2" ]])
+            ->add('picture', TextType::class, [
+                'label' => 'Picture',
+                'attr' => ['class' => 'form-control w-75', 'style' => 'margin-bottom:2rem;', 'placeholder'=>'Picture']])
+            ->add('capacity',  NumberType::class, [
+                'label' => 'Number of People',
+                'attr' => ['class' => 'form-control w-75', 'style' => 'margin-bottom:2rem;', 'placeholder'=>'Number of People']
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email Address',
+                'attr' => ['class' => 'form-control w-75', 'style' => 'margin-bottom:2rem;', 'placeholder'=>'Email Address']])
+            ->add('phone_number', NumberType::class, [
+                'label' => 'Phone Number',
+                'attr' => ['class' => 'form-control w-75', 'style' => 'margin-bottom:2rem;', 'placeholder'=>'Phone Number']
+            ])
+            ->add('city', ChoiceType::class, [
+                'choices' => ['Vienna' => 'Vienna', 'Upper Austria' => 'Upper Austria', 'Lower Austria' => 'Lower Austria', 'Styria' => 'Styria', 'Carinthia' =>'Carinthia', 'Vorarlberg'=>"Vorarlberg"],
+                'attr' => ['class' => 'form-control w-75', 'style' => 'margin-bottom:2rem']
+            ])
+            ->add('street_name',  TextType::class, [
+                'label' => 'Street Name',
+                'attr' => ['class' => 'form-control w-75', 'style' => 'margin-bottom:2rem;', 'placeholder'=>'Street Name']
+            ])
+            ->add('house_number',  NumberType::class, [
+                'label' => 'House Number',
+                'attr' => ['class' => 'form-control w-75', 'style' => 'margin-bottom:2rem;', 'placeholder'=>'House Number']
+            ])
+            ->add('zip_code', NumberType::class, [
+                'label' => 'Zip Code',
+                'attr' => ['class' => 'form-control w-75', 'style' => 'margin-bottom:2rem;', 'placeholder'=>'Zip Code']
+            ])
+            ->add('event_url', UrlType::class, [
+                'label' => 'Event Website',
+                'attr' => ['class' => 'form-control w-75', 'style' => 'margin-bottom:2rem;', 'placeholder'=>'Event Website']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
